befef4b898970717d2272b34f1f04b39a17c7fec
Bickenn <162151633+Bickenn@users.noreply.github.com>
2024-04-22 20:55:18 -0400

    Add files via upload

--- CNIT581Project/CNIT581FINAL/CNIT581FINALPROJECT/researchweb/admin.py
+++ CNIT581Project/CNIT581FINAL/CNIT581FINALPROJECT/researchweb/admin.py
@@ -4,4 +4,6 @@ from django.contrib import admin
 
 from .models import *
 
+#adds the user table in the admin site
+#username for the admin site is apuls and the password is Password123
 admin.site.register(User)
--- CNIT581Project/CNIT581FINAL/CNIT581FINALPROJECT/researchweb/forms.py
+++ CNIT581Project/CNIT581FINAL/CNIT581FINALPROJECT/researchweb/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from .models import User
 from .models import authUser
 
+#creates the form to be used in the addUser page
 class addUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = '__all__'
 
+#creates the form to be added in in adminLogin page
 class adminLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
